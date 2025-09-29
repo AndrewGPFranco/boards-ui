be4c26d04b1b20509f86c89b78ca23a694c0ab30
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-09-28 22:52:02 -0300

    feat: add mensagem de boas vindas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,9 @@
       <router-link :to="{ name: 'UserRegister' }">Register</router-link>
       <router-link :to="{ name: 'Login' }">Login</router-link>
     </div>
+    <div v-else>
+      <p class="font-bold">Bem vindo a Plataforma </p>
+    </div>
   </section>
 </template>
 
